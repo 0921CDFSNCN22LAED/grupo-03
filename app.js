737e6357dd2e2d729caec9497071ee18986ea428
nico89221@hotmail.com <nico89221@hotmail.com>
2021-10-30 11:38:16 -0300

    se crearon las carpetas public/css y public/img, ademas tambien se agrego la carpeta views que contiene index.html, se crearon los package.json y package-lock.json

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("prueba");
+const express = require("express");
+
+const app = express();
+
+const path = require("path");
+
+app.use(express.static(path.join(__dirname,"/public")))
+
+app.listen(3000,()=>{
+
+    console.log("Servidor encendido");
+})
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/index.html"))
+})
