5d0da702198e545957836fffe4ca7833034b4468
nico89221@hotmail.com <nico89221@hotmail.com>
2021-11-05 15:48:53 -0300

    se agregaron imagenes en la carpeta img, se modifico los estilos tanto del desktop como mobile.

--- app.js
+++ app.js
@@ -30,3 +30,7 @@ app.get("/register",(req,res)=>{
 app.get("/login",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/login.html"))
 })
+
+app.get("/armaTuPc",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/armaTuPc.html"))
+})
