df40fa94a0527aa7414cf9015d294e022375175d
Adrian de Inés <asdic1800@yahoo.com.ar>
2021-11-28 23:07:54 -0300

    products_json users.json register and maincontrollers

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -5,6 +5,10 @@ const productsFilePath = path.join(__dirname, "../data/products.json");
 const productsFileText = fs.readFileSync(productsFilePath, "utf-8");
 const products = JSON.parse(productsFileText); //ARRAY de PRODUCTOS
 
+const usersFilePath = path.join(__dirname, "../data/users.json");
+const usersFileText = fs.readFileSync(usersFilePath, "utf-8");
+const users = JSON.parse(usersFileText); //ARRAY de PRODUCTOS
+
 const controller = {
     home: (req, res) => {
         res.render("index");
