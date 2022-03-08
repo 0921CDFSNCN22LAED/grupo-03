88eaf2ccf7e31fde4ca6b1bf30ad7a21b183cf0e
nico89221@hotmail.com <nico89221@hotmail.com>
2022-03-08 19:05:07 -0300

    Creacion de ruta api para obtener el buscador de productos por categoria

--- dashboard/src/components/ContentProduct.js
+++ dashboard/src/components/ContentProduct.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-mport { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 function ContenProduct(props)  {
  let {id} = useParams();
